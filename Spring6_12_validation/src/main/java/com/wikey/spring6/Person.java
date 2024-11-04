746b9abd3b8bf243e2406ea9f4d384769eb55b4b
Wikey Cao <caoweiqi@haiyisoft.com>
2024-11-04 17:42:42 +0800

    study spring6

---
+++ Spring6_12_validation/src/main/java/com/wikey/spring6/Person.java
@@ -0,0 +1,27 @@
+package com.wikey.spring6;
+
+/**
+ * @ Author：Wikey Cao
+ * @ Date：04-11-2024
+ * @ Description：
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
